2f0f81f71ec68b2ad35d3fb5fa3e5e0f44106ff3
niamhmccoo <niamhmccooey@gmail.com>
2019-08-05 22:22:33 +0100

    Added search engine :)

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import './App.css';
 
 //  1. Started off by building our App component & giving it an empty array:
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-      this.state = {
-        people: [],
-      }
-      this.getPeople = this.getPeople.bind(this); // can do this with an arrow function too - note: why do it here?
+    this.state = {
+      people: [],
+    }
+    this.getPeople = this.getPeople.bind(this); // can do this with an arrow function too - note: why do it here?
   }
 
   // 2. Once we make an API request we use that info to change the component's state & populate the people property in the App class above, with whatever came back from the API request, i.e. 'people: response.data.results' below:
-  
-  getPeople(){
-    return axios.get("https://swapi.co/api/people/", { headers: { 'Content-Type': 'application/json', 'Accept': 'application/json'}}) // Trying to fix the html split-second hard-reloading problem by passing axios an object that sets the headers to the appropriate format:
-    .then((response) => {
-      console.log(response.data.results);
-      this.setState( { people: response.data.results } )
-    })
-    .catch((error) => {
-      console.error(error);
-    })
+
+  getPeople() {
+    return axios.get("https://swapi.co/api/people/")
+      .then((response) => {
+        console.log(response.data.results);
+        this.setState({people: response.data.results})
+      })
+      .catch((error) => {
+        console.error(error);
+      })
   }
 
   componentDidMount() {
@@ -33,9 +33,9 @@ class App extends React.Component {
   //  3. We then passed that 'people' data to the list component below - heading over to List.jsx - through the prop called people:
   render() {
     const {people} = this.state;
-    return (
-      <div className="App">
-        <List people={people}></List> 
+    return ( 
+    <div className = "App" >
+      <List people = {people} ></List>
       </div>
     );
   }
--- src/List.jsx
+++ src/List.jsx
@@ -5,24 +5,39 @@ class List extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            search: ''
+            search: '' // Object setting a default state for our search
         };
     }
 
-    render() {
-        const people = this.props.people; // Remember: 'people' is an array
-        return (
+    updateSearch(event) {
+        this.setState({search: event.target.value.substr(0, 20)});
+    }
 
+    render() {
+        const people = this.props.people.filter(
+            (people) => {
+                return people.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+            }
+        ); // Remember: 'people' is an array
+
+        return (
             <div className="App">
-                <input type="text" />
+                <nav>
+                    <h1>💫 Use the force 💫</h1>
+                    <h3>Start typing to find your Star Wars buddies...</h3>
+                </nav>
+                    <input type="text" 
+                        value={this.state.search}
+                        onChange={this.updateSearch.bind(this)} />
                 {
                     people.map((p) => { // 4. Here we're mapping over all of the 'people' array items, then returning the character name; a property inside the array. Then we pass that item to 'CharInfo' so we can use it independently, taking info from each character in the CharInfo.jsx file.
                         console.log(p);
                         return (
-                            <div key={p.url}>
+                            <div class="results">
                                 <h1 className="char-name">{p.name}</h1>
                                 <CharInfo charInfo={p} />
                             </div>
+                            
                         )
                     })
                 }
